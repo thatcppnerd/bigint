14098985eb29f29709f4dee691e41f98e7015c5c
thatcppnerd <welcometoheccmp4@gmail.com>
2025-04-07 11:47:20 -0500

    WIP WIP

--- bigint.cc
+++ bigint.cc
@@ -1,10 +1,10 @@
 #include "bigint.hh"
 
-#define FULL_TEMPLATE template <int num_blocks, typename blocktype, bool sign_enable>
+#define FULL_TEMPLATE template <int num_blocks, typename block_t, bool sign_enable>
 
 
 FULL_TEMPLATE 
-void bigint<num_blocks, blocktype, sign_enable>::operator+=(const int val)
+void bigint<num_blocks, block_t, sign_enable>::operator+=(const block_t val)
 {
     blocks[0] += val;
 
@@ -12,8 +12,19 @@ void bigint<num_blocks, blocktype, sign_enable>::operator+=(const int val)
     if (blocks[0] < (blocks[0] - val)) blocks[1]++;
 }
 
+FULL_TEMPLATE
+void bigint<num_blocks, block_t, sign_enable>::operator+=(const bigint<num_blocks, block_t, sign_enable> val)
+{
+    for (int i = 0 ; i < num_blocks - 1 ; i++)
+    {
+        blocks[i] += val;
+
+        if(blocks[i] < (blocks[0] - val)) blocks[i + 1]++;
+    }
+}
+
 FULL_TEMPLATE 
-void bigint<num_blocks, blocktype, sign_enable>::operator-=(const int val)
+void bigint<num_blocks, block_t, sign_enable>::operator-=(const block_t val)
 {
     blocks[0] -= val;
 
@@ -21,23 +32,32 @@ void bigint<num_blocks, blocktype, sign_enable>::operator-=(const int val)
     if (blocks[0] > (blocks[0] + val)) blocks[1]--;
 }
 
+FULL_TEMPLATE
+void bigint<num_blocks, block_t, sign_enable>::operator-=(const bigint<num_blocks, block_t, sign_enable> val)
+{
+    for (int i = 0 ; i < num_blocks - 1 ; i++)
+    {
+        blocks[i] -= val;
 
+        if(blocks[i] > (blocks[0] + val)) blocks[i + 1]--;
+    }
+}
+
+FULL_TEMPLATE 
+void bigint<num_blocks, block_t, sign_enable>::operator*=(const block_t val)
+{
+    
+}
 
 
 #pragma region CONSTRUCTORS
 
 FULL_TEMPLATE
-bigint<num_blocks, blocktype, sign_enable>::bigint(const int val)
-{
-    blocks = {};
-    *(int*)&blocks = val;
-}
-
-FULL_TEMPLATE
-bigint<num_blocks, blocktype, sign_enable>::bigint(const blocktype val)
+bigint<num_blocks, block_t, sign_enable>::bigint(const block_t val)
 {
     blocks = {val};
 }
+
 
 
 
--- bigint.hh
+++ bigint.hh
@@ -2,59 +2,51 @@
 #define __BIGINT_HH__
 
 
+template <int num_blocks, typename block_t, bool sign_enable> class bigint;
 
-
-template <int num_blocks, typename blocktype, bool sign_enable>
+template <int num_blocks, typename block_t, bool sign_enable>
 class bigint
 {
     protected:
-        blocktype blocks[num_blocks];
+        block_t blocks[num_blocks];
 
     public:
-        void operator+=(const int val);
-        void operator+=(const blocktype val);
-        void operator+=(const bigint<num_blocks, blocktype, sign_enable> val);
+        void operator+=(const block_t val);
+        void operator+=(const bigint<num_blocks, block_t, sign_enable> val);
 
-        void operator-=(const int val);
-        void operator-=(const blocktype val);
-        void operator-=(const bigint<num_blocks, blocktype, sign_enable> val);
+        void operator-=(const block_t val);
+        void operator-=(const bigint<num_blocks, block_t, sign_enable> val);
 
-        void operator*=(const int val);
-        void operator*=(const blocktype val);
-        void operator*=(const bigint<num_blocks, blocktype, sign_enable> val);
+        void operator*=(const block_t val);
+        void operator*=(const bigint<num_blocks, block_t, sign_enable> val);
 
-        void operator/=(const int val);
-        void operator/=(const blocktype val);
-        void operator/=(const bigint<num_blocks, blocktype, sign_enable> val);
+        void operator/=(const block_t val);
+        void operator/=(const bigint<num_blocks, block_t, sign_enable> val);
 
         void operator<<=(const int shift);
         void operator>>=(const int shift);
 
-        void operator&=(const int val);
-        void operator&=(const blocktype val);
-        void operator&=(const bigint<num_blocks, blocktype, sign_enable> val);
+        void operator&=(const block_t val);
+        void operator&=(const bigint<num_blocks, block_t, sign_enable> val);
 
-        void operator|=(const int val);
-        void operator|=(const blocktype val);
-        void operator|=(const bigint<num_blocks, blocktype, sign_enable> val);
+        void operator|=(const block_t val);
+        void operator|=(const bigint<num_blocks, block_t, sign_enable> val);
 
-        void operator^=(const int val);
-        void operator^=(const blocktype val);
-        void operator^=(const bigint<num_blocks, blocktype, sign_enable> val);
+        void operator^=(const block_t val);
+        void operator^=(const bigint<num_blocks, block_t, sign_enable> val);
 
 
-        const bigint<num_blocks, blocktype, sign_enable> operator+(const int val);
-        const bigint<num_blocks, blocktype, sign_enable> operator+(const blocktype val);
-        const bigint<num_blocks, blocktype, sign_enable> operator+(const bigint<num_blocks, blocktype, sign_enable> val);
+        const bigint<num_blocks, block_t, sign_enable> operator+(const block_t val);
+        const bigint<num_blocks, block_t, sign_enable> operator+(const bigint<num_blocks, block_t, sign_enable> val);
 
-        const bigint<num_blocks, blocktype, sign_enable> operator-(const int val);
-        const bigint<num_blocks, blocktype, sign_enable> operator-(const blocktype val);
-        const bigint<num_blocks, blocktype, sign_enable> operator-(const bigint<num_blocks, blocktype, sign_enable> val);
+        const bigint<num_blocks, block_t, sign_enable> operator-(const block_t val);
+        const bigint<num_blocks, block_t, sign_enable> operator-(const bigint<num_blocks, block_t, sign_enable> val);
 
-        bigint(const int val);
-        bigint(const blocktype val);
-        bigint(const blocktype val...);
+        bigint(const block_t val);
 };
+
+template <int num_blocks, bool sign_enable>
+class bigint<num_blocks, short>
 
 
 
