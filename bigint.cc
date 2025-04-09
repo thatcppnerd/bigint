543e1d5a77d392bd97c09f828817ad14fcea9c9d
thatcppnerd <welcometoheccmp4@gmail.com>
2025-04-08 22:09:44 -0500

    WIP 3

--- bigint.cc
+++ bigint.cc
@@ -1,10 +1,11 @@
 #include "bigint.hh"
 
-#define FULL_TEMPLATE template <int num_blocks, typename block_t, bool sign_enable>
+#define FULL_TEMPLATE template <int __num_blocks, typename __block_t, bool __sign_enable>
+#define TEMPLATE_NO_BT template <int __num_blocks, bool __sign_enable>
 
 
 FULL_TEMPLATE 
-void bigint<num_blocks, block_t, sign_enable>::operator+=(const block_t val)
+void __bigint_common<__num_blocks, __block_t, __sign_enable>::operator+=(const __block_t val)
 {
     blocks[0] += val;
 
@@ -13,9 +14,9 @@ void bigint<num_blocks, block_t, sign_enable>::operator+=(const block_t val)
 }
 
 FULL_TEMPLATE
-void bigint<num_blocks, block_t, sign_enable>::operator+=(const bigint<num_blocks, block_t, sign_enable> val)
+void __bigint_common<__num_blocks, __block_t, __sign_enable>::operator+=(const bigint<__num_blocks, __block_t, __sign_enable> val)
 {
-    for (int i = 0 ; i < num_blocks - 1 ; i++)
+    for (int i = 0 ; i < __num_blocks - 1 ; i++)
     {
         blocks[i] += val;
 
@@ -24,7 +25,7 @@ void bigint<num_blocks, block_t, sign_enable>::operator+=(const bigint<num_block
 }
 
 FULL_TEMPLATE 
-void bigint<num_blocks, block_t, sign_enable>::operator-=(const block_t val)
+void __bigint_common<__num_blocks, __block_t, __sign_enable>::operator-=(const __block_t val)
 {
     blocks[0] -= val;
 
@@ -33,9 +34,9 @@ void bigint<num_blocks, block_t, sign_enable>::operator-=(const block_t val)
 }
 
 FULL_TEMPLATE
-void bigint<num_blocks, block_t, sign_enable>::operator-=(const bigint<num_blocks, block_t, sign_enable> val)
+void __bigint_common<__num_blocks, __block_t, __sign_enable>::operator-=(const bigint<__num_blocks, __block_t, __sign_enable> val)
 {
-    for (int i = 0 ; i < num_blocks - 1 ; i++)
+    for (int i = 0 ; i < __num_blocks - 1 ; i++)
     {
         blocks[i] -= val;
 
@@ -43,24 +44,58 @@ void bigint<num_blocks, block_t, sign_enable>::operator-=(const bigint<num_block
     }
 }
 
-FULL_TEMPLATE 
-void bigint<num_blocks, block_t, sign_enable>::operator*=(const block_t val)
-{
-    
+FULL_TEMPLATE
+void __bigint_common<__num_blocks, __block_t, __sign_enable>::operator*=(const __block_t val)
+{    
+    // multiply 
+    for (int i = 0 ; i < __num_blocks ; i++)
+    {
+        blocks[i] *= val;
+    }
+
+    __block_t mid[__num_blocks - 1] = {};
+
+    __block_t &&high_mask = (__block_t(0) - 1) << ((sizeof(__block_t) * 8)/2);
+
+
+    for (int i = 0 ; i < __num_blocks - 1 ; i++)
+    {
+        mid[i] = blocks[i] + blocks[i + 1];
+    }
 }
 
+FULL_TEMPLATE
+void __bigint_common<__num_blocks, __block_t, __sign_enable>::operator/=(const __block_t val)
+{
+
+}
 
 #pragma region CONSTRUCTORS
 
 FULL_TEMPLATE
-bigint<num_blocks, block_t, sign_enable>::bigint(const block_t val)
+__bigint_common<__num_blocks, __block_t, __sign_enable>::__bigint_common(const __block_t val)
 {
     blocks = {val};
 }
 
+FULL_TEMPLATE
+__bigint_common<__num_blocks, __block_t, __sign_enable>::__bigint_common(const int count, const __block_t val...)
+{
+    blocks = {};
 
+    for (int i = 0 ; i < count ; i++)
+    {
+        blocks[i] = val[i];
+    }
+}
 
+FULL_TEMPLATE
+bigint<__num_blocks, __block_t, __sign_enable>::bigint(const __block_t val) : __bigint_common<__num_blocks, __block_t, __sign_enable>(val)
+{}
 
+FULL_TEMPLATE
+bigint<__num_blocks, __block_t, __sign_enable>::bigint(const int count, const __block_t val...) : __bigint_common<__num_blocks, __block_t, __sign_enable>(count, val)
+{}
 
 
 #pragma endregion
--- bigint.hh
+++ bigint.hh
@@ -1,53 +1,62 @@
 #ifndef __BIGINT_HH__
 #define __BIGINT_HH__
 
+/// @brief An arbitrary-length integer
+/// @tparam __block_t The type of the blocks that make up the integer. Only operable integer types accepted.
+/// @tparam __num_blocks The number of blocks that make up the integer.
+/// @tparam __sign_enable Toggles signed or unsigned operations.
+template <int __num_blocks, typename __block_t, bool __sign_enable> class bigint;
 
-template <int num_blocks, typename block_t, bool sign_enable> class bigint;
-
-template <int num_blocks, typename block_t, bool sign_enable>
-class bigint
+template <int __num_blocks, typename __block_t, bool __sign_enable>
+struct __bigint_common
 {
-    protected:
-        block_t blocks[num_blocks];
+    __block_t blocks[__num_blocks];
 
-    public:
-        void operator+=(const block_t val);
-        void operator+=(const bigint<num_blocks, block_t, sign_enable> val);
+    const int&& num_blocks = __num_blocks;
+    using block_t = __block_t;
+    const bool&& sign_enable = __sign_enable;
 
-        void operator-=(const block_t val);
-        void operator-=(const bigint<num_blocks, block_t, sign_enable> val);
+    void operator+=(const __block_t val);
+    void operator+=(const bigint<__num_blocks, __block_t, __sign_enable> val);
 
-        void operator*=(const block_t val);
-        void operator*=(const bigint<num_blocks, block_t, sign_enable> val);
+    void operator-=(const __block_t val);
+    void operator-=(const bigint<__num_blocks, __block_t, __sign_enable> val);
 
-        void operator/=(const block_t val);
-        void operator/=(const bigint<num_blocks, block_t, sign_enable> val);
+    virtual void operator*=(const __block_t val);
+    virtual void operator*=(const bigint<__num_blocks, __block_t, __sign_enable> val);
 
-        void operator<<=(const int shift);
-        void operator>>=(const int shift);
+    virtual void operator/=(const __block_t val);
+    virtual void operator/=(const bigint<__num_blocks, __block_t, __sign_enable> val);
 
-        void operator&=(const block_t val);
-        void operator&=(const bigint<num_blocks, block_t, sign_enable> val);
+    void operator<<=(const int shift);
+    void operator>>=(const int shift);
 
-        void operator|=(const block_t val);
-        void operator|=(const bigint<num_blocks, block_t, sign_enable> val);
+    void operator&=(const __block_t val);
+    void operator&=(const bigint<__num_blocks, __block_t, __sign_enable> val);
 
-        void operator^=(const block_t val);
-        void operator^=(const bigint<num_blocks, block_t, sign_enable> val);
+    void operator|=(const __block_t val);
+    void operator|=(const bigint<__num_blocks, __block_t, __sign_enable> val);
+
+    void operator^=(const __block_t val);
+    void operator^=(const bigint<__num_blocks, __block_t, __sign_enable> val);
 
 
-        const bigint<num_blocks, block_t, sign_enable> operator+(const block_t val);
-        const bigint<num_blocks, block_t, sign_enable> operator+(const bigint<num_blocks, block_t, sign_enable> val);
+    const bigint<__num_blocks, __block_t, __sign_enable> operator+(const __block_t val);
+    const bigint<__num_blocks, __block_t, __sign_enable> operator+(const bigint<__num_blocks, __block_t, __sign_enable> val);
 
-        const bigint<num_blocks, block_t, sign_enable> operator-(const block_t val);
-        const bigint<num_blocks, block_t, sign_enable> operator-(const bigint<num_blocks, block_t, sign_enable> val);
+    const bigint<__num_blocks, __block_t, __sign_enable> operator-(const __block_t val);
+    const bigint<__num_blocks, __block_t, __sign_enable> operator-(const bigint<__num_blocks, __block_t, __sign_enable> val);
 
-        bigint(const block_t val);
+    __bigint_common(const __block_t val);
+    __bigint_common(const int count, const __block_t val...);
 };
 
-template <int num_blocks, bool sign_enable>
-class bigint<num_blocks, short>
 
-
+template <int __num_blocks, typename __block_t, bool __sign_enable>
+struct bigint : public __bigint_common<__num_blocks, __block_t, __sign_enable>
+{
+    bigint(const __block_t val);
+    bigint(const int count, const __block_t val...);
+};
 
 #endif
